bda6670b707df2e6985dcbd528a625dd56fc3997
Márcio C.G. <marcio.goulart@gmail.com>
2025-10-18 20:32:02 -0300

    remove unused import for jakarta.ws.rs.DefaultValue

--- src/main/java/io/github/marciocg/payment/api/PaymentResource.java
+++ src/main/java/io/github/marciocg/payment/api/PaymentResource.java
@@ -16,7 +16,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
